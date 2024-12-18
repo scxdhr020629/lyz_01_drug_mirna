2c44a1e62cfcc624cb456e0bad2c27c78c3967d6
鹤纸千 <1282026764@qq.com>
2024-12-18 16:24:42 +0800

    第二部分正确代码 但是排序还有问题

--- test.py
+++ test.py
@@ -178,3 +178,141 @@ for i in range(1, 2):
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 # 第二部分 进行预测
 # ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# 删去 rog auc
+
+def predicting(model, device, loader):
+    model.eval()
+    total_probs = []
+    sample_indices = []
+    total_labels = []
+
+    logging.info('Making predictions for {} samples...'.format(len(loader.dataset)))
+    with torch.no_grad():
+        for batch_idx, data in enumerate(loader):
+
+            data = data.to(device)
+            output = model(data)
+            probs = output.cpu().numpy()
+            indices = np.arange(len(probs)) + batch_idx * loader.batch_size
+
+            total_probs.extend(probs)
+            sample_indices.extend(indices)
+            total_labels.extend(data.y.view(-1, 1).cpu().numpy())
+
+    total_probs = np.array(total_probs).flatten()
+    sample_indices = np.array(sample_indices).flatten()
+    total_labels = np.array(total_labels).flatten()
+
+    # accuracy = accuracy_score(total_labels, (total_probs >= 0.5).astype(int))
+    # precision = precision_score(total_labels, (total_probs >= 0.5).astype(int), zero_division=1)
+    # recall = recall_score(total_labels, (total_probs >= 0.5).astype(int))
+    # f1 = f1_score(total_labels, (total_probs >= 0.5).astype(int))
+
+    # logging.info(f"Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}")
+    #
+    # roc_auc = roc_auc_score(total_labels, total_probs)
+    # precision_vals, recall_vals, _ = precision_recall_curve(total_labels, total_probs)
+    # sorted_indices = np.argsort(recall_vals)
+    # recall_vals = recall_vals[sorted_indices]
+    # precision_vals = precision_vals[sorted_indices]
+    # pr_auc = auc(recall_vals, precision_vals)
+    #
+    # logging.info(f"ROC AUC: {roc_auc:.4f}")
+
+    # return total_probs, sample_indices, accuracy, precision, recall, f1, pr_auc, total_labels
+    # return total_probs, sample_indices, accuracy, precision, recall, f1, pr_auc
+    return total_probs, sample_indices
+
+
+# 先随便写的
+def save_top_30_predictions(probs, indices, file_name='top_30_predictions_01.csv'):
+    # Sort by probability (in descending order)
+    sorted_indices = np.argsort(probs)[::-1]  # sort in descending order
+    sorted_probs = probs[sorted_indices]
+    # sorted_labels = labels[sorted_indices]
+    sorted_indices = indices[sorted_indices]
+
+    # Create a DataFrame to save the top 30 predictions
+    top_30_df = pd.DataFrame({
+        'Index': sorted_indices[:30],
+        'Probability': sorted_probs[:30],
+        # 'True_Label': sorted_labels[:30]
+    })
+
+    # Save to CSV
+    top_30_df.to_csv(file_name, index=False)
+    logging.info(f'Top 30 predictions saved to {file_name}')
+
+
+
+
+
+
+
+
+
+
+modeling =GCNNetmuti
+
+model_st = modeling.__name__
+
+cuda_name = "cuda:0"
+if len(sys.argv) > 3:
+    cuda_name = "cuda:" + str(int(sys.argv[1]))
+print('cuda_name:', cuda_name)
+
+TRAIN_BATCH_SIZE = 64
+TEST_BATCH_SIZE = 64
+LR = 0.0005
+LOG_INTERVAL = 160
+NUM_EPOCHS =100
+
+print('Learning rate: ', LR)
+print('Epochs: ', NUM_EPOCHS)
+
+# Main program: iterate over different datasets
+
+print('\nrunning on ', model_st + '_')
+
+
+log_filename = f'training_1.log'
+
+logging.basicConfig(filename=log_filename, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+
+processed_data_file_test = 'data/processed/last/' + '_mytest1'+'.pt'
+if ( (not os.path.isfile(processed_data_file_test))):
+    print('please run process_data_old.py to prepare data in pytorch format!')
+else:
+    # train_data = TestbedDataset(root='data', dataset='_train1')
+    # test_data = TestbedDataset(root='data', dataset='_test1')
+
+    test_data = TestbedDataset(root='data', dataset='last/_mytest1')
+
+    test_loader = DataLoader(test_data, batch_size=TEST_BATCH_SIZE,shuffle=False,drop_last=True)
+
+    # training the model
+    device = torch.device(cuda_name if torch.cuda.is_available() else "cpu")
+    model = modeling().to(device)
+    model_path = "model_GCNNetmuti_.model"
+    model.load_state_dict(torch.load(model_path))
+
+
+
+
+    loss_fn = nn.BCELoss()  # for classification
+    # optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    # best_acc= 0
+    # best_epoch = -1
+
+    result_file_name = 'result_' + model_st + '_' + '.csv'
+
+
+    probs, indices = predicting(model, device, test_loader)
+    save_top_30_predictions(probs, indices)
+
+
